26f947fa961820d1db6f2a09d7076299741a0996
alexatboost <akhayesmba@gmail.com>
2026-03-11 10:34:23 -0700

    Update module 2 to 2026

--- module_2_the_pricing_pulley/story_content/user.js
+++ module_2_the_pricing_pulley/story_content/user.js
@@ -1,7 +1,16 @@
-function ExecuteScript(strId)
+window.InitUserScripts = function()
 {
-  switch (strId)
-  {
-  }
-}
-
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
